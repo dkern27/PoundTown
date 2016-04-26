a9ee92daaba193ca5a8afe076cbcebcb0a0786f3
harryk333 <Harry@wl-dhcp198-7.Mines.EDU>
2016-04-26 14:14:22 -0600

    Level now generate list of animals

--- src/FarmGame/Level.java
+++ src/FarmGame/Level.java
@@ -1,20 +1,27 @@
 package FarmGame;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Level {
 	
 	private String name;
-	private ArrayList<Location> locations;
-	private boolean levelComplete;
 	private String backgroundFile;
+	private ArrayList<Location> locations;
+	private int numDigits;
+	private ArrayList<Animal> animals;
+	private AnimalType animalType;
+	private String drawFile; //For animal
+	private boolean levelComplete;
+	
 	
 	public Level( String name, String backgroundFile) {
 		this.name = name;
 		this.backgroundFile = backgroundFile;
 		levelComplete = false;
 		
-		//Initialize locations
+		//Initialize animals
+		generateAnimals();
 		
 	}
 	
@@ -23,11 +30,27 @@ public class Level {
 		return gui;
 	}
 	
+	public void generateAnimals() {
+		
+		//Caluclate the maximum possible value given the number of binary digits
+		double maxNum = Math.pow(2, numDigits) - 1;
+		
+		//Make an array of all the possible animals
+		for( int i=0; i < maxNum; i++ ) {
+			animals.add( new Animal( animalType, drawFile, i, numDigits ));
+		}
+		
+	}
+	
 
 ////// Getters and Setters //////
 	
 	public ArrayList<Location> getLocations() {
 		return locations;
+	}
+	
+	public ArrayList<Animal> getAnimalsList() {
+		return animals;
 	}
 
 	public String getName() {
