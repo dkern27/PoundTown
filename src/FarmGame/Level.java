860f77630556e8f95ec44544ffc4cd277ae82f08
Hanna Barringer <hbarring@mines.edu>
2016-04-26 20:53:13 -0600

    off by one

--- src/FarmGame/Level.java
+++ src/FarmGame/Level.java
@@ -42,6 +42,7 @@ public class Level {
 	public void generateAnimals() {
 		
 		//Calculate the maximum possible value given the number of binary digits
+		double maxNum = Math.pow(2, (numDigits))-1; 
 		
 		//Make an array of all the possible animals
 		for( int i=0; i < maxNum; i++ ) {
