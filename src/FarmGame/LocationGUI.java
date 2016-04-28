8c9b1a28251ae271fba999810d120ff345040302
Dylan Kern <dkern@mines.edu>
2016-04-27 18:32:40 -0600

    ok now

--- src/FarmGame/LocationGUI.java
+++ src/FarmGame/LocationGUI.java
@@ -1,9 +1,26 @@
 package FarmGame;
 
-public class LocationGUI {
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
-	public LocationGUI() {
-		// TODO Auto-generated constructor stub
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class LocationGUI extends JPanel{
+
+	public LocationGUI(String fileName) {
+		BufferedImage backgroundImage = null;
+		try {
+			backgroundImage = ImageIO.read( new File(fileName) );
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		JLabel picLabel = new JLabel(new ImageIcon( backgroundImage ));
+		add(picLabel);
 	}
 
 }
