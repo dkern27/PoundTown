f4adc5234b2fcf9c5c2056543e325fb103cc2958
Hanna Barringer <hbarring@mines.edu>
2016-04-27 20:53:01 -0600

    Added bg files to locations

--- src/FarmGame/LocationType.java
+++ src/FarmGame/LocationType.java
@@ -1,10 +1,10 @@
 package FarmGame;
 
 public enum LocationType {
-	POND(""), // These all need files 
-	FOREST(""), 
-	FIELD(""), 
-	VOLCANO("");
+	POND("BackgroundImages/Lake.jpg"), // These all need files 
+	FOREST("BackgroundImages/Forest.jpg"), 
+	FIELD("BackgroundImages/Pasture.jpg"), 
+	VOLCANO("BackgroundImages/Volcano.jpg");
 	
 	private final String fileName;
 	
