c8f624f16ad60a0733e569a063aa9e523df630e2
Hanna Barringer <hbarring@mines.edu>
2016-04-29 09:47:54 -0600

    added map click box

---
+++ src/FarmGame/MapClickBox.java
@@ -0,0 +1,17 @@
+package FarmGame;
+
+public class MapClickBox {
+
+	private int height;
+	private int width;
+	private int x;
+	private int y;
+	
+	MapClickBox(int h, int w, int y, int x, Location location){
+		height = h;
+		width = w;
+		this.x = x;
+		this.y = y;
+	}
+	
+}
